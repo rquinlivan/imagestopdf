9f2444866b812eee50f25ba87ca4ac15132846e2
Robert Quinlivan <robert@rquinlivan.net>
2021-04-20 12:50:30 -0500

    Ignore DS_Store files

--- pdfstuff.py
+++ pdfstuff.py
@@ -8,7 +8,10 @@ images = sorted(images)
 print(f"Processing the following images in the images directory: {images}")
 pdf = FPDF()
 for image in images:
-    pdf.add_page()
-    pdf.image(f"images/{image}", 0, 0, 210, 297)
+    if 'DS_Store' not in image:
+        pdf.add_page()
+        pdf.image(f"images/{image}", 0, 0, 210, 297)
+    else:
+        print("Skipping DS_Store file")
 pdf.output("output.pdf", "F")
 
